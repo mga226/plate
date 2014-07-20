2d0bf00807f01c244ab55540fa679c7cd24beb9d
Mike Acreman <mike.acreman@gmail.com>
2014-07-20 15:28:01 -0400

    renamed some files

--- sandbox.php
+++ sandbox.php
@@ -1,202 +1,55 @@
-<?php 
+<?php
 
-define('PLATE_REGEX', '
-			/
-		
-			# will never write a regex this complicated again
-			# (and will hopefully never have to revisit this one) -MGA
-		
-			(?P<pair>
-				{
-					(?P<pair_tag>
-						(?:
-							(?>
-								(?:[^{}\ ])+
-								|
-								{(?:[^{}\ ])+}
-							)+	
-						)
-					)
-					(?P<pair_params>\ ([^{}]|(?:
-					
-					
-						# single reproduced here
-					
-						{
-							(?:
-								(
-									#(?>
-										(?:[^{}\ ])+
-										|
-										{(?:[^{}\ ])+}
-									#)+
-		
-								)
-							)
-							(?:\ ([^{}]|(?>{[^{}]*}))*)?
-						}					
-					
-						# end of single reproduced
-					
-					))*)?
-				}
-				# (?P<pair_content>((?>pair) | (?>single) | [\s\S])*?)
-				# replaced with the following to circumvent catastrophic backtracking:
-				(?P<pair_content>((?>pair) | (?>single) | (?>[\s\S])*?))
-				{\/\3}
-			)
-			|
-			(?P<single>
-				{
-					(?P<single_tag>
-						(
-							#(?>
-								(?:[^{}\ ])+
-								|
-								{(?:[^{}\ ])+}
-							#)+	
-						)
-					)
-					(?P<single_params>\ ([^{}]|(?>{[^{}]*}))*)?
-				}
-			)
-			/x');
+require_once('vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php');
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+ 
+$loader = new UniversalClassLoader();
+$loader->registerNamespace('Plate',__DIR__);
+#$loader->registerNamespace("Symfony\Component" => "/path/to/symfony/components");
+#$loader->registerNamespace("Monolog" => "path/to/monolog/src/");
+#$loader->registerPrefix("Zend_", "path/to/zend/library");
+$loader->register();
+
+
+//
+$plate = new Plate\Parser();
+$plate->setTemplate('{title}');
+$plate->setData(array('title'=>'MyArray'));
+$plate->parse();
+echo $plate->getBuffer();
+#spl_autoload_extensions(".php");
+#spl_autoload_register();
+#use 
 
 
 
-$string  = 'Hello {world this="that" that="the-other"}. My name is {name}.';
-
-
-class PlateCallback{
-
-	function run($match){
-		$directive = PlateDirectiveFactory::create($match);
-	}
-}
-
-class PlateDirectiveFactory{
-	static function create(Array $regexMatch){
-	
-		if( !empty($regexMatch['pair_tag']) ){
-			$tag = $regexMatch['pair_tag'];
-			$params = $regexMatch['pair_params'];
-			$text = !empty($regexMatch['pair_content']) $regexMatch['pair_content'] : FALSE;
-
-		} elseif ( !empty($regexMatch['single_tag']) ){
-			$tag = $regexMatch['single_tag'];
-			$params = $regexMatch['single_params'];
-			$text = FALSE;
-		} else {
-			throw new InvalidArgumentException('No tag provided.');
-		
-		}
-
-		$isPlugin = FALSE;
-
-		if($tag[0] == ':'){
-			$isPlugin = TRUE;
-			$tag = substr($tag, 1);
-		}
-
-		$params = $this->parseParams($params);
-
-		if($isPlugin){
-			return new PlateDirective_Plugin($tag, $params, $text);
-		} else {
-			return new PlateDirective_Data($tag, $params, $text);
-		}
-
-	}
-
-	protected function parseParams(String $params){
-		if(preg_match_all('/(?P<param>[\w:]+)\s*=\s*(["\']?)(?P<value>.*?)(\2)/',$params, $params)){
-			$params = array_combine($params['param'], $params['value']);
-		} else {
-			$params = array();
-		}
-
-		return $params;
-	}
-
-
-}
-
-
-abstract class PlateDirective {
-
-	private string $text;
-	private var $tag;
-	private array $params;
-
-	function __construct($tag, $params, $text){
-		$this->tag = $tag;
-		$this->params = $params;
-		$this->text = $text;
-	}
-
-	abstract function run();
-
-	public function getTag(){
-		return $this->tag;
-	}
-
-	public function getParams(){
-		return $this->params;
-	}
-
-	public function getText(){
-		return $this->text;
-	}
-
-}
-
-class PlateDirective_Data extends PlateDirective{
-
-
-	public function run($data){
-		$datapoint = $data->getDataOrPseudodata($this->getTag());
-		return $datapoint->processForTemplate( $this->getText(), $this->getParams() );
-	}
-}
-
-class PlateDirective_Plugin extends PlateDirective{
-
-	public function run($data, $bufferIn){
-		if(function_exists($this->getTag())){
-			call_user_func_array(
-				$this->getTag(), 
-				array( $this->getText(), $this->getParams() )
-			)
-		}
-	}
-
-}
 
 
 /*
+require ('classes/Datapoint.php');
+require ('classes/Dataset.php');
+require ('classes/PlateParser.php');
+require ('classes/PlateDirective.php');
 
-	WRITE THIS
+require ('classes/Plate.php');
 
+
+$dp = DatapointFactory::create('Hello World');
+
+
+$set = new Dataset(
+	array(
+		'title' => 'My Title',
+		'content' => 'My Content!',
+	)
+);
+
+//print $set->getDatapoint('content')."\n";
+
+$plate = new Plate($set, '{numberOfFields} Title is {title}, content is {content}. {:some_fucking_plugin:reverse}. Heres something else: {title:reverse}');
+
+$plate->parse();
+
+echo $plate->getBuffer();
 */
-class PlateDirective_IfBlock extends PlateDirective{
-
-	private string $condition;
-
-	public function run($data){
-
-	}
-
-
-}
-
-
-
-function callback($match){
-	print_r($match);
-
-}
-
-$string = preg_replace_callback(PLATE_REGEX, 'callback', $string);
-
-#preg_match_all(PLATE_REGEX, $string, $matches);
-print_r($matches);
